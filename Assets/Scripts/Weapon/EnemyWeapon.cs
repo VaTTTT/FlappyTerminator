866a6f51c2978d8dac0d7a9a6514c5f0f0d129ff
Alexey Razumeichenko <84139032+VaTTTT@users.noreply.github.com>
2024-04-11 14:02:00 +0300

    Update

--- Assets/Scripts/Weapon/EnemyWeapon.cs
+++ Assets/Scripts/Weapon/EnemyWeapon.cs
@@ -6,7 +6,6 @@ public class EnemyWeapon : Weapon
     private void OnEnable()
     {
         _shootRoutine = StartCoroutine(nameof(Shoot));
-        Debug.Log("Coroutine is started " + _shootRoutine);
     }
 
     private void OnDisable()
